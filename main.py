9dee8adba79e496105963723c75fd46a6ae4e50b
zuzell <62251533+zuzell@users.noreply.github.com>
2020-04-01 21:41:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
-#main.py
-
 from fastapi import FastAPI
 
-app=FastAPI()
+from pydantic import BaseModel
+
+app = FastAPI()
+# to see what funny will come
+app.counter = 0
+
+
+class HelloResp(BaseModel):
+    msg: str
 
 @app.get("/")
 def root():
-	return {"message": "Hello World"}
+    return {"message": "Hello World"}
+
+
+@app.get('/counter')
+def counter():
+    app.counter += 1
+    return str(app.counter)
+
+
+@app.get("/hello/{name}", response_model=HelloResp)
+async def read_item(name: str):
+    return HelloResp(msg=f"Hello {name}")
