26f3ea687af61868ef773746a388e76296231f91
zuzell <62251533+zuzell@users.noreply.github.com>
2020-04-02 20:06:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,23 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
-# to see what funny will come
-app.counter = 0
-
-
-class HelloResp(BaseModel):
-    msg: str
 
 @app.get("/")
 def root():
     return {"message": "Hello World during the coronavirus pandemic!"}
+
+class GiveMeSomethingRq(BaseModel):
+    first_key: str
+
+
+class GiveMeSomethingResp(BaseModel):
+    received: Dict
+    constant_data: str = "python jest super"
+
+
+@app.post("/dej/mi/coś", response_model=GiveMeSomethingResp)
+def receive_something(rq: GiveMeSomethingRq):
+    return GiveMeSomethingResp(received=rq.dict())
 
 
 
