6f09ecb7ec30cd9049bdd1e96bee9c5a65902582
zuzell <62251533+zuzell@users.noreply.github.com>
2020-04-25 20:16:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,104 @@
-from fastapi import FastAPI, Response, HTTPException, Request
-from hashlib import sha256
-from fastapi.templating import Jinja2Templates
-from pydantic import BaseModel
-from fastapi.responses import JSONResponse
-from starlette.responses import RedirectResponse
+from _sha256 import sha256
 
+from typing import Dict
+
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from pydantic import BaseModel
+from fastapi import FastAPI, Request, Response, status, Depends, HTTPException
+from starlette.responses import RedirectResponse, JSONResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-app.num = 0
-app.count = -1
-app.users = {"trudnY": "PaC13Nt", "admin": "admin"}
-app.secret = "secret"
-app.tokens = []
-patlist = []
+app.counter: int = 0
+
+
+class Patient(BaseModel):
+    name: str
+    surname: str
+    id: str = 0
+
+
+class PatientsResp(BaseModel):
+    response: dict
+
+
 templates = Jinja2Templates(directory="templates")
+app.storage: Dict[int, Patient] = {}
+app.tokens = []
+
+security = HTTPBasic()
+app.secret_key = "3586551867030721809738080201689944348810193121742430128090228167"
+
+
+def check_login(credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = secrets.compare_digest(credentials.username, "trudnY")
+    correct_password = secrets.compare_digest(credentials.password, "PaC13Nt")
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username + ":" + credentials.password
+
 
 @app.get("/")
 def root():
-	return {"message": "Hello World during the coronavirus pandemic!"}
+    return {"message": "Hello World during the coronavirus pandemic!"}
 
 
-@app.get("/welcome")
-def welcome_to_the_jungle():
-	return {"message": "Welcome to the jungle! We have funny games!"}
+@app.api_route(path="/welcome", methods=["GET"])
+def welcome(request: Request, auth: str = Depends(check_login)):
+    return templates.TemplateResponse("greeting.html", {"request": request, "user": auth.split(':', 1)[0]})
+
+
+@app.post("/patient")
+async def add_patient(response: Response, patient: Patient, auth: str = Depends(check_login)):
+    patient.id = "id_" + str(app.counter)
+    app.storage[app.counter] = patient
+    app.counter += 1
+    response.status_code = status.HTTP_302_FOUND
+    response.headers["Location"] = f"/patient/{app.counter - 1}"
+
+
+@app.get("/patient")
+def get_patients(response: Response, auth: str = Depends(check_login)):
+    resp = {}
+    for x in app.storage.values():
+        resp[x.id] = {'name': x.name, 'surname': x.surname}
+    if resp:
+        return JSONResponse(resp)
+    response.status_code = status.HTTP_204_NO_CONTENT
+
+
+@app.get("/patient/{pk}")
+def get_patient(pk: int, auth: str = Depends(check_login)):
+    if pk in app.storage:
+        return app.storage.get(pk)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.delete("/patient/{pk}")
+def get_patient(pk: int, response: Response, auth: str = Depends(check_login)):
+    if pk in app.storage:
+        app.storage.pop(pk, None)
+        response.status_code = status.HTTP_204_NO_CONTENT
 
 
 @app.post("/login")
-def login_to_app(user: str, passw: str, response: Response,request: Request ):
-	if user in app.users and passw == app.users[user]:
-		s_token = sha256(bytes(f"{user}{passw}{app.secret}", encoding='utf8')).hexdigest()
-		app.tokens += s_token
-		response.set_cookie(key="session_token",value=s_token)
-		response = RedirectResponse(url='/welcome')
-		print('logged in')
-		return templates.TemplateResponse("loginpage.html", {"request": request})
-	else:
-		raise HTTPException(status_code=401)
+async def login(response: Response, login_pass: str = Depends(check_login)):
+    response.headers["Location"] = "/welcome"
+    response.status_code = status.HTTP_302_FOUND
+    secret_token = sha256(bytes(f"{login_pass.split(':', 1)[0]}{login_pass.split(':', 1)[1]}{app.secret_key}",
+                                encoding='utf8')).hexdigest()
+    app.tokens += secret_token
+    response.set_cookie(key="session_token", value=secret_token)
+    return response
+
+
+@app.post("/logout")
+def logout(response: Response, auth: str = Depends(check_login)):
+    response.status_code = status.HTTP_302_FOUND
+    response.headers["Location"] = "/"
+    response.delete_cookie("session_token")
+    return response
